922ade25165a60e807188c75cb2b6bf923058907
Marcus <marcus@deltalima.org>
2023-06-13 00:48:05 +0200

    changes

--- include/packages.inc.sh
+++ include/packages.inc.sh
@@ -16,7 +16,7 @@ gnome-shell-extension-panel-osd
 yaru-theme-gnome-shell yaru-theme-gtk yaru-theme-icon yaru-theme-sound
 yaru-theme-unity
 gnome-software-plugin-flatpak
-brasero dconf-editor
+brasero dconf-editor network-manager-openvpn-gnome
 thunderbird thunderbird-l10n-de"
 
 # install admin and dev tools
