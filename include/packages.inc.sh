bb0a583efe0014bed57c416005714115fcfee0af
DeltaLima <marcus@deltalima.org>
2023-06-13 10:41:46 +0200

    „include/packages.inc.sh“ ändern

--- include/packages.inc.sh
+++ include/packages.inc.sh
@@ -3,9 +3,11 @@
 declare -A packages
 
 # install base desktop stuff
-packages[base]="ttf-mscorefonts-installer fonts-ubuntu fonts-ubuntu-console 
+packages[base]="ttf-mscorefonts-installer fonts-ubuntu fonts-ubuntu-console fonts-liberation2
+fonts-noto-core fonts-noto-extra fonts-noto-ui-core fonts-noto-ui-extra 
+fonts-dejavu fonts-hack 
 flatpak flatpak-xdg-utils linux-headers-amd64 plymouth build-essential
-p7zip-full unrar unzip shotwell neofetch" 
+p7zip-full unrar unzip shotwell neofetch ecryptfs-utils" 
 
 # install gnome base
 packages[gnome]="task-gnome-desktop task-german-desktop 
@@ -19,10 +21,13 @@ gnome-software-plugin-flatpak
 brasero dconf-editor network-manager-openvpn-gnome
 thunderbird thunderbird-l10n-de"
 
+packages[xfce]="gnome-keyring seahorse mugshot elementary-xfce-icon-theme 
+lightdm-settings lightdm-gtk-greeter-settings"
+
 # install admin and dev tools
 packages[admin]="htop iotop sysstat lm-sensors git mc vim btop btrfs-progs 
 curl wget debootstrap geany geany-plugins lnav mtr-tiny ncdu nmap ppp 
-pandoc pwgen remmina rsync screen socat stress strace tcpdump ufw borgbackup borgmatic "
+pandoc pwgen remmina rsync screen socat stress strace tcpdump ufw borgbackup borgmatic colordiff"
 
 # install nice programs
 packages[nice]="wine:i386 winetricks chromium dosbox gimp vlc barrier audacity
