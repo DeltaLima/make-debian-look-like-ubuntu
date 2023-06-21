f96a1e1aa5aa0c8328bc2957ead666a9f75c888a
Marcus <marcus@deltalima.org>
2023-06-21 21:46:17 +0200

    add MIT license

--- make-debian-look-like-ubuntu.sh
+++ make-debian-look-like-ubuntu.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Title: make-debian-look-like-ubuntu.sh
+# Description: This script performs all necessary steps to make a Debian Gnome desktop look like an Ubuntu desktop. Also it installs flatpak with flathub.org repository enabled and Firefox from there.
+# Author: DeltaLima
+# Date: 21.06.2023
+# Version: 1.0
+# Usage: bash make-debian-look-like-ubuntu.sh
+# 
+# Copyright 2023 DeltaLima
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# 
+#
+
 arguments="$@"
 
 # define the $packages[] array
